8c5ba347c729189df5da57975afc9f12f61173a9
정원준 <jeongwonjun0718@gmail.com>
2022-03-18 19:47:01 +0900

    버튼을 눌러 텍스트 바꾸기

--- lib/main.dart
+++ lib/main.dart
@@ -14,27 +14,31 @@ class MyApp extends StatefulWidget {
 
 class _MyApp extends State<MyApp> {
   var switchValue = false;
-  // This widget is the root of your application.
+  String test = 'hello'; // 버튼에 들어갈 텍스트 입력
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
-          // visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         darkTheme: ThemeData.light(),
         home: Scaffold(
           body: Center(
-            child: Switch(
-                value: switchValue,
-                onChanged: (value) {
-                  setState(() {
-                    print(value);
-                    switchValue = value;
-                  });
-                }),
-          ),
+              child: ElevatedButton(
+                  child: Text('$test'),
+                  onPressed: () {
+                    if (test == 'hello') {
+                      setState(() {
+                        test = 'flutter';
+                      });
+                    } else {
+                      setState(() {
+                        test = 'hello';
+                      });
+                    }
+                  })),
         ));
   }
 }
