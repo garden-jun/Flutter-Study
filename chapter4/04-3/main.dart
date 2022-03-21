7a2a99cc98812cdc6f8e15d371eed81ccf58a414
정원준 <79080825+garden-jun@users.noreply.github.com>
2022-03-21 11:08:59 +0900

    뺄셈, 곱셈, 나눗셈 기능 추가하기 (DropdownButton)

--- chapter4/04-3/main.dart
+++ chapter4/04-3/main.dart
@@ -25,9 +25,23 @@ class WidgetApp extends StatefulWidget {
 }
 
 class _WidgetExampleState extends State<WidgetApp> {
+  List _buttonList = ['더하기', '빼기', '곱하기', '나누기'];
+  List<DropdownMenuItem<String>> _dropDownMenuItems =
+      new List.empty(growable: true);
+  String? _buttonText;
+
   String sum = '';
   TextEditingController value1 = TextEditingController();
   TextEditingController value2 = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    for (var item in _buttonList) {
+      _dropDownMenuItems.add(DropdownMenuItem(value: item, child: Text(item)));
+    }
+    _buttonText = _dropDownMenuItems[0].value;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +55,7 @@ class _WidgetExampleState extends State<WidgetApp> {
             Padding(
               padding: EdgeInsets.all(15),
               child: Text(
-                '근데 사칙연산만 됨 ㅋㅋ\n어차피 님들 공학용계산기로 사칙연산밖에 안하잖아요 ㅋㅋ\n\n\n\n결과: $sum',
+                '근데 사칙연산만 됨 ㅋㅋ\n어차피 님들 공학용계산기로 사칙연산밖에 안하잖아요ㅋㅋ\n\n\n\n결과: $sum',
                 style: TextStyle(fontSize: 20),
               ),
             ),
@@ -59,18 +73,40 @@ class _WidgetExampleState extends State<WidgetApp> {
               padding: EdgeInsets.all(15),
               child: ElevatedButton(
                   child: Row(
-                    children: <Widget>[Icon(Icons.add), Text('더하기')],
+                    children: <Widget>[Icon(Icons.add), Text(_buttonText!)],
                   ),
                   style: ButtonStyle(
                       backgroundColor: MaterialStateProperty.all(Colors.amber)),
                   onPressed: () {
                     setState(() {
-                      int result = int.parse(value1.value.text) +
-                          int.parse(value2.value.text);
+                      var value1Int = double.parse(value1.value.text);
+                      var value2Int = double.parse(value2.value.text);
+                      var result;
+                      if (_buttonText == '더하기') {
+                        result = value1Int + value2Int;
+                      } else if (_buttonText == '빼기') {
+                        result = value1Int - value2Int;
+                      } else if (_buttonText == '곱하기') {
+                        result = value1Int * value2Int;
+                      } else if (_buttonText == '나누기') {
+                        result = value1Int / value2Int;
+                      }
                       sum = '$result';
                     });
                   }),
             ),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: DropdownButton(
+                items: _dropDownMenuItems,
+                onChanged: (String? value) {
+                  setState(() {
+                    _buttonText = value;
+                  });
+                },
+                value: _buttonText,
+              ),
+            )
           ]),
         ),
       ),
